18ac729b9a5f7c22f53e696faf42f32b2aee6c51
Marcelo Domínguez <69964857+Sa4dUs@users.noreply.github.com>
2024-04-24 10:14:30 +0200

    add minor visual changes to start screen

--- src/components/StartScreen.jsx
+++ src/components/StartScreen.jsx
@@ -1,12 +1,18 @@
-import scss from "../styles/StartScreen.module.scss"
+import scss from "../styles/StartScreen.module.scss";
+import weaponSprite from "../../public/assets/weapon-axe.png";
 
-const StartScreen = ({action}) => {
+const StartScreen = ({ action }) => {
     return (
         <div className={scss.container}>
-            <h1>Bits-Of-Battle</h1>
-            <button onClick={action}>Play</button>
+            <h1 className={scss.title}>Bits-Of-Battle</h1>
+            <div className={scss.arcadePanel}>
+                <div className={scss.gameSprites}>
+                    <img className={scss.weapon} src={weaponSprite} alt="Weapon Sprite" />
+                </div>
+                <button className={scss.playButton} onClick={action}>Insert Coin to Play</button>
+            </div>
         </div>
     );
 };
 
-export default StartScreen
+export default StartScreen;
