a74ebebf5263dc2306375bfcb1da79c04d181821
CuB1z <cub1zdev@gmail.com>
2024-04-25 11:35:27 +0200

    update startscreen style

--- src/components/StartScreen.jsx
+++ src/components/StartScreen.jsx
@@ -7,7 +7,7 @@ const StartScreen = ({ action }) => {
             <h1 className={"title"}>Bits-Of-Battle</h1>
             <div className={"arcadePanel"}>
                 <div className={"gameSprites"}>
-                    <img className={"weapon"} src={weaponSprite} alt="Weapon Sprite" />
+                    <img className={"weapon sprite"} src={weaponSprite} alt="Weapon Sprite" />
                 </div>
                 <button className={"playButton"} onClick={action}>Insert Coin to Play</button>
             </div>
