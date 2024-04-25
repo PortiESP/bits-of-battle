7df9e2c3bf861cc92cb4e09307e82028883acdc2
PRTI <portiesp@gmail.com>
2024-04-25 22:19:22 +0200

    main menu sprites

--- src/components/StartScreen.jsx
+++ src/components/StartScreen.jsx
@@ -2,6 +2,7 @@ import { useState } from "react"
 import "../styles/StartScreen.css"
 import MainMenu from "./menu_scenes/MainMenu"
 import ControlsMenu from "./menu_scenes/ControlsMenu"
+import HowToPlayMenu from "./menu_scenes/HowToPlayMenu"
 
 const StartScreen = ({ setShowStartScreen }) => {
     const [scene, setScene] = useState(0)
@@ -11,7 +12,8 @@ const StartScreen = ({ setShowStartScreen }) => {
         <div className={"container"}>
             {
                 (scene === 0 && <MainMenu {...funcs} />) ||
-                (scene === 1 && <ControlsMenu {...funcs} />)
+                (scene === 1 && <HowToPlayMenu {...funcs} />) || 
+                (scene === 2 && <ControlsMenu {...funcs} />)
             }
         </div>
     )
--- src/components/menu_scenes/MainMenu.jsx
+++ src/components/menu_scenes/MainMenu.jsx
@@ -1,20 +1,29 @@
 import weaponSprite from "../../../public/assets/weapon-axe.png"
 import "../../styles/StartScreenMenus/MainMenu.css"
+import Button from "../Button"
+import MainMenuPlayer from "./main/MainMenuPlayer"
 
 export default function MainMenu(props) {
     return (
         <>
             <h1 className={"title"}>Bits Of Battle</h1>
-            <div className={"arcadePanel"}>
-                <div className={"gameSprites"}>
-                    <img className={"weapon sprite"} src={weaponSprite} alt="Weapon Sprite" />
+            <div className="main-wrapper">
+                <MainMenuPlayer id={1} src={"../../../public/assets/characters/blueNinja/character-blueNinja.png"}/>
+                <div className={"arcadePanel"}>
+                    <div className={"gameSprites"}>
+                        <img className={"weapon sprite"} src={weaponSprite} alt="Weapon Sprite" />
+                    </div>
+                    <Button color="#ff3333"  onClick={() => props.setShowStartScreen(false)}>
+                        Play
+                    </Button>
+                    <Button color="#6e03F5" onClick={() => props.setScene(1)}>
+                        How to Play!
+                    </Button>
+                    <Button color="#3ea325" onClick={() => props.setScene(2)}>
+                        Controls
+                    </Button>
                 </div>
-                <button className={"playButton"} onClick={() => props.setShowStartScreen(false)}>
-                    Play
-                </button>
-                <button className={"playButton"} onClick={() => props.setScene(1)}>
-                    Controls
-                </button>
+                <MainMenuPlayer id={2} src={"../../../public/assets/characters/greenNinja/character-greenNinja.png"}/>
             </div>
         </>
     )
