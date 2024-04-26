1be265702e7e22eb5bd1e56b4ad1b6c4379ea38e
PRTI <portiesp@gmail.com>
2024-04-26 12:00:25 +0200

    default buttons button

--- src/components/menu_scenes/ControlsMenu.jsx
+++ src/components/menu_scenes/ControlsMenu.jsx
@@ -19,6 +19,12 @@ export default function ControlsMenu(props) {
         window.customControls2 = controls2
     }, [controls2])
 
+    const resetControls = () => {
+        setControls1(CONST.CONTROLS_P1)
+        setControls2(CONST.CONTROLS_P2)
+    }
+
+
 
     return <>
         <h1>Controls</h1>
@@ -26,6 +32,7 @@ export default function ControlsMenu(props) {
             <KbdControls id={1} controls={controls1} setControls={setControls1}/>
             <KbdControls id={2} controls={controls2} setControls={setControls2}/>
         </div>
+        <span className="sub-btn" onClick={resetControls}>Set default configuration</span>
         <div className="ctr-menu-buttons">
             <Button onClick={() => props.setScene(0)}>Back to main menu</Button>
         </div>
